ede352c1d20378b45a817e5d161728b9f44237d6
Evila Santos <evilavsantos@gmail.com>
2017-03-01 23:41:19 +0000

    I made changee

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
+$("#studentButton").click(function(){
+var students = ["Evila", "Kaci", "Abby","Richard","Tim" ]
+var rand = Math.floor(Math.random()*students.length);
+console.log(rand);
+console.log(students[rand])
 
+});
+
+$("#teacherButton").click(function(){
+var teacher = ["Matt", "Sam", "Samuel","Trey"]
+var rand = Math.floor(Math.random()*teacher.length);
+console.log(rand);
+console.log(teacher[rand])
+});
 
 
 });
